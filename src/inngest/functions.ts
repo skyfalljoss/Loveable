6b9c560a41882f9e5c20126db00a7ccb19154897
Phong Nguyen <skyfalljoss@gmail.com>
2025-10-15 23:46:47 -0400

    feat: job AI with inngest ai toolkits

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,24 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world-name-function" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    //imaging this a download step
-    await step.sleep("wait-a-moment", "30s");
-    // imagine this is a processing step, like a model inference or a database query
-    await step.sleep("wait-a-moment", "10s");
-    //imagine this is a upload step
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+
+    const summarizer = createAgent({
+        name: "summarizer",
+        system: "You are an expert summarizer.  Your summarize in 2 words.",
+        model: openai({ model: "gpt-4o" }),
+      });
+
+    const { output } = await summarizer.run(
+        `Summarize the following text: ${event.data.value}`
+    )
+
+    console.log(output)
+
+    return { output };
   },
 );
