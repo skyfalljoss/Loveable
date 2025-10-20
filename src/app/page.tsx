db498a3bc09021339001c1d7da9034e8a48ee86d
Phong Nguyen <skyfalljoss@gmail.com>
2025-10-20 10:15:46 -0400

    feat: message chore: changed function names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
-
-import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
-import {toast} from "sonner";
 import {Input} from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {toast} from "sonner";
 import { useState } from "react";
+
 
 
 const Page = () => {
@@ -14,9 +15,10 @@ const Page = () => {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({
+  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
     onSuccess:()=>{
-      toast.success("Background job start")
+      toast.success("Message created")
     }
   }));
  
@@ -25,10 +27,12 @@ const Page = () => {
     <div className="p-4 max-w-7xl mx-auto">
       <Input value ={value} onChange ={(e)=> setValue(e.target.value)}>
       </Input>
-      <Button disabled = {invoke.isPending } onClick={() => invoke.mutate({ value: value})}>
+      <Button 
+      disabled = {createMessage.isPending } 
+      onClick={() => createMessage.mutate({ value: value})}>
         Invoke Inngest
       </Button>
-  
+    {/* {JSON.stringify(messages, null, 2)} */}
     </div>
   );
 };
