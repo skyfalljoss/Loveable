6927ff5f99f979a80f00263d83f3ba0b20f2a4e6
Phong Nguyen <skyfalljoss@gmail.com>
2025-11-05 22:11:12 -0500

    13: code -view tree view

---
+++ src/components/code-view/index.tsx
@@ -0,0 +1,32 @@
+import prism from "prismjs";
+
+import {useEffect} from "react";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-typescript";
+
+import "./code-theme.css";
+
+interface Props{
+    code: string;
+    language: string;
+}
+
+export const CodeView = ({
+    code,
+    language
+}: Props) => {
+
+    useEffect(() => {
+        prism.highlightAll();
+    }, [code, language]);
+
+    return (
+        <pre className="p-2 bg-transparent rounded-none border-none m-0 text-xs language-${language}">
+            <code className={`language-${language} `}>
+                {code}
+            </code>
+        </pre>
+    )
+}
