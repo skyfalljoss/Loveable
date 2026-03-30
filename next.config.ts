dec0f23cbdfdf245433b3053339681583158cb9c
Phong Nguyen <skyfalljoss@gmail.com>
2026-03-30 13:31:14 -0400

    fix: mark opentelemetry packages as server external

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@opentelemetry/winston-transport",
+    "@opentelemetry/instrumentation-winston",
+    "inngest",
+  ],
 };
 
 export default nextConfig;
